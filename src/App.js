4702ee7a211d8df89b86ddc53846971933dac300
Geo <megagog19@gmail.com>
2020-05-25 20:54:08 +0300

    Alter App.js (checkpoint)
    * import components
    * add sign handlers
    * alter render method to display components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content';
 
 class App extends Component {
   state = { logged: false }
   
+  sign_in = () => {
+    this.setState(
+      {logged: true}
+    )
+  }
+
+  sign_out = () => {
+    this.setState(
+      {logged: false}
+    )
+  }
+
+  sign_up = () => {
+
+  }
+
   componentDidMount() {
 
   }
 
   render() {
     return (
-      <div>
-        Hi
+      <div id="app">
+        <Header logged={this.state.logged}/>
+        <Content logged={this.state.logged} sign={{sign_in: this.sign_in, sign_out: this.sign_out, sign_up: this.sign_up}}/>
+        <Footer logged={this.state.logged}/>
       </div>
     );
   }
