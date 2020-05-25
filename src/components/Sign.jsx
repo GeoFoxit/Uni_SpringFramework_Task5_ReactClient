4adc8ea50c92d2624dd486a9968dbdffc11d02b6
Geo <megagog19@gmail.com>
2020-05-25 12:46:49 +0300

    Small fixes
    Prepare project

---
+++ src/components/Sign.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sign extends Component {
+    render() {
+        return (
+            <div>
+                Sign
+            </div>
+        );
+    }
+}
+
+export default Sign;
