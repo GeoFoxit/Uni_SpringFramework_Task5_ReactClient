65f90741c1f25e4ad475e5610501cdbe408b7778
Geo <megagog19@gmail.com>
2020-05-25 20:53:09 +0300

    Alter sign component
    * add sign forms

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 
 class Sign extends Component {
     render() {
+        const {sign_in, sign_out, sign_up} = this.props.sign;
+
         return (
-            <div>
-                Sign
+            <div className="sign">
+                <div>
+                    <h2>Sign In</h2>
+                    <input type="email" name="" id="" placeholder="E-mail"/>
+                    <input type="password" name="" id="" placeholder="Password"/>
+                    <button onClick={sign_in}>Submit</button>
+                </div>
+                <hr/>
+                <div>
+                    <h2>Sign Up</h2>
+                    <input type="text" name="" id="" placeholder="Name"/>
+                    <input type="email" name="" id="" placeholder="E-mail"/>
+                    <input type="password" name="" id="" placeholder="Password"/>
+                    <input type="password" name="" id="" placeholder="Repeat password"/>
+                    <button onClick={sign_up}>Submit</button>
+                </div>
             </div>
         );
     }
