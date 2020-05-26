02bae20fe8cea1e1acc3f07ed4be55e2e6d36609
Geo <megagog19@gmail.com>
2020-05-26 18:31:57 +0300

    Add components
    * add Profile component
    * add Contacts component
    * add Favorite component
    * add Cart component
    * add Compare component

---
+++ src/components/Compare.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Compare extends Component {
+    render() {
+        return (
+            <div>
+                Compare
+            </div>
+        );
+    }
+}
+
+export default Compare;
